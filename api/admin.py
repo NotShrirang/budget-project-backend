69f425ae6d2553c758b467da603a694fdc75d860
atharvabhide <atharvabhide91@gmail.com>
2023-08-08 18:27:44 +0530

    feat: add models to admin site

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from api.models import (
+    CollegeUser,
+    Department,
+    Transaction,
+    Activity,
+)
 
-# Register your models here.
+admin.site.site_header = 'Budget Project Admin'
+
+admin.site.register(CollegeUser)
+admin.site.register(Department)
+admin.site.register(Transaction)
+admin.site.register(Activity)
