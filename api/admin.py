da32dd01962781e74deff959c41de86b6b904083
atharvabhide <atharvabhide91@gmail.com>
2023-10-02 12:10:25 +0530

    fix: hash user password

--- api/admin.py
+++ api/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CollegeUser
 from api.models import (
     CollegeUser,
     Department,
@@ -8,7 +10,42 @@ from api.models import (
 
 admin.site.site_header = 'Budget Project Admin'
 
-admin.site.register(CollegeUser)
+class CollegeUserAdmin(UserAdmin):
+    model = CollegeUser
+    list_display = (
+        'username',
+        'email',
+        'department',
+        'privilege',
+        'is_active',
+        'is_staff',
+        'is_superuser',
+    )
+    list_filter = (
+        'department',
+        'privilege',
+        'is_active',
+        'is_staff',
+        'is_superuser',
+    )
+    search_fields = ('username', 'email')
+    ordering = ('email',)
+
+    fieldsets = (
+        (None, {'fields': ('email', 'username', 'password')}),
+        ('Department Info', {'fields': ('department',)}),
+        ('Privilege Info', {'fields': ('privilege',)}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'username', 'password1', 'password2', 'department', 'privilege'),
+        }),
+    )
+
+admin.site.register(CollegeUser, CollegeUserAdmin)
 admin.site.register(Department)
 admin.site.register(Transaction)
 admin.site.register(Activity)
